a7d863fe88fca813845abbfad06cfc4acd6448f0
lv <lv@lvdeMacBook-Pro.local>
2026-06-07 22:06:18 +0800

    feat: save config as GameWatch.ini next to exe

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <QApplication>
 #include <QIcon>
+#include <QSettings>
+#include <QDir>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
@@ -8,6 +10,15 @@ int main(int argc, char *argv[])
     app.setApplicationName("GameWatch");
     app.setOrganizationName("GameWatch");
     app.setApplicationVersion("1.0.0");
+
+    // 使用 INI 格式，配置文件保存在 exe 同级目录
+    QSettings::setDefaultFormat(QSettings::IniFormat);
+    QString iniPath = QDir(QCoreApplication::applicationDirPath())
+                          .filePath("GameWatch.ini");
+    QSettings::setPath(QSettings::IniFormat, QSettings::UserScope, iniPath);
+    // 强制加载使路径生效
+    QSettings settings;
+    Q_UNUSED(settings);
 
     MainWindow w;
     w.show();
